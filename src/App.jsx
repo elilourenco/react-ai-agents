56d9445d28b2f54eeea515c15abdd0340f4297a6
elilourenco <lourencomonica318@gmail.com>
2025-09-26 21:37:49 +0100

    add the creat the ai agents

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import {Chat} from "./components/Chat/Chat"
+import { GoogleGenAI } from "@google/genai"
 import styles from  "./App.module.css"
 import { useState } from "react"
 import { Controls } from "./components/Controls/Controls"
 
+// Initialize Google Generative AI client
+const googleai= new GoogleGenAI(import.meta.env.VITE_GOOGLE_AI_API_KEY)
+const gemini= googleai.getGenerativeModel({model: "gemini-1.5-flash"})
+const chat= gemini.startChat({history: []})
+
+
+// Main Application Component
 function App() {
   const [messages, setMessages]= useState([])
 
+
+// Function to add a new message to the chat
+  function  addMessage(message){
+    setMessages((prevMessages) =>[...prevMessages,message])
+  }
+
   
-  function handleContentSend(content){
+   async function handleContentSend(content){
     
-   setMessages((prevMessages) =>[...prevMessages,{ content,role: "user"}])
+   addMessage({role: "user", content})
+
+
+   try {
+
+    const result= await chat.sendMessage(content);
+    addMessage({role: "assistant", content: result.response.text()})
+    
+   } catch (error) {
+    console.error(error);
+    addMessage({role: "system", content: "Sorry i couldn`t process your request. Please try again!"} )
+    
+   }
 
   }
 
