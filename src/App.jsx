137d745a20814a6d0fc435b0e69c16f8f3a65112
elilourenco <lourencomonica318@gmail.com>
2025-09-27 18:39:43 +0100

    add the page and add the configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import {Chat} from "./components/Chat/Chat"
-import { Assistant } from "./assistants/googleai"
+import { Assistant } from "./assistants/openai"
 import styles from  "./App.module.css"
 import { useState } from "react"
 import { Controls } from "./components/Controls/Controls"
+import { Loader } from "./components/Loader/Loader"
 
  
-
 
 // Main Application Component
 function App() {
 
   const assistant=new Assistant();
   const [messages, setMessages]= useState([])
+
+  const [isLoading, setIsLoading]= useState(false)  
 
 
 // Function to add a new message to the chat
@@ -21,12 +23,12 @@ function App() {
 
   // Function to handle sending user content and receiving AI response
   async function handleContentSend(content){
+    setIsLoading(true)
     
     addMessage({role: "user", content})
 
-
     try {
-      const result= await assistant.chat(content);
+      const result= await assistant.chat(content,messages);
 
     
     addMessage({role: "assistant", content: result})
@@ -35,12 +37,15 @@ function App() {
     console.error(error);
     addMessage({role: "system", content: "Sorry i couldn`t process your request. Please try again!"} )
     
+   }finally{
+    setIsLoading(false)
    }
 
   }
 
   return (
     <div  className={styles.App}>
+      {isLoading && <Loader/> }
       <header  className={styles.Header}>
         <img  className={styles.Logo} src="/robot.svg" />
         <h2 className={styles.Title}>AI Chatbot</h2>
